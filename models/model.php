a5d598cafae38d1b46b0c5031b06516e9e7c6db1
PtspluS <pierre.marez@isen.yncrea.fr>
2021-03-05 10:25:44 +0100

    add port

--- models/model.php
+++ models/model.php
@@ -5,9 +5,10 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'demo');
+define('DB_PORT', 3306);
 
 /* Attempt to connect to MySQL database */
-$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
 
 // Check connection
 if($mysqli === false){
